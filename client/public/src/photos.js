10e8dd08e434c38663008d09a03ac1164f836135
ljcucc <ljcucc@gmail.com>
2022-06-01 18:53:48 +0800

    change to grid layout

--- client/public/src/photos.js
+++ client/public/src/photos.js
@@ -10,8 +10,11 @@ class PhotosGrid extends LitElement{
   }
 
   .box{
-    width: var(--side);
-    height:var(--side);
+    /* width: var(--side);
+    height:var(--side); */
+    width: 100%;
+    height: 100%;
+    aspect-ratio: 1 / 1;
     background: #e0e0e0;
     transition: box-shadow 0.35s;
     object-fit: cover;
@@ -19,17 +22,39 @@ class PhotosGrid extends LitElement{
 
   .boxes{
     display: grid;
-    grid-auto-rows: var(--side);
-    grid-template-columns: repeat(5, 1fr);
+    /* grid-auto-rows: var(--side); */
+    /* grid-template-columns: repeat(var(--each-line), 1fr); */
+    --grid: repeat(auto-fit, minmax(170px, 2fr));
+    grid-template-columns: var(--grid);
+    grid-template-rows: var(--grid);
     box-sizing: content-box;
     gap: 10px;
     padding-top: 100px;
     margin: auto;
     justify-content: center;
+    box-sizing: content-box;
   }
 
   .box:hover{
     box-shadow: rgba(0,0,0,.35) 0px 0px 4px;
+  }
+
+  @media only screen and (min-width: 1000px){
+    :host{
+      --each-line: 6 !important;
+    }
+  }
+
+  @media only screen and (max-width: 800px) and (min-width: 500px){
+    :host{
+      --each-line: 4;
+    }
+  }
+
+  @media only screen and (max-width: 500px){
+    :host{
+      --each-line: 3;
+    }
   }
   `;
 
@@ -62,6 +87,7 @@ class PhotosGrid extends LitElement{
     <button @click="${this.loadPage}">load more</button>
     `;
   }
+
 }
 
 customElements.define("photos-grid", PhotosGrid);
