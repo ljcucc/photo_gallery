66602478343143bcba5b33b50a87a4d487e5e530
ljcucc <ljcucc@gmail.com>
2022-06-02 19:22:22 +0800

    disable using service worker

--- client/public/src/index.js
+++ client/public/src/index.js
@@ -26,25 +26,25 @@ let isLocalhost = ()=>location.hostname == "localhost" || location.hostname == "
 // redirect to https if using http
 if (location.protocol == "http:" && !(isLocalhost())) location.protocol = "https:";
 
-const enableServiceWorker = !isLocalhost();
+// const enableServiceWorker = !isLocalhost();
 
-if (enableServiceWorker) {
-  // working with service-worker in order to make PWA installation works
-  window.addEventListener('load', () => {
-    async function registerSW() {
-      if ('serviceWorker' in navigator) {
-        try {
-          await navigator.serviceWorker.register('./service-worker.js');
-        } catch (e) {
-          console.log('ServiceWorker registration failed. Sorry about that.', e);
-        }
-      } else {
-        console.log('Your browser does not support ServiceWorker.');
-      }
-    }
-    registerSW();
-  });
-}
+// if (enableServiceWorker) {
+//   // working with service-worker in order to make PWA installation works
+//   window.addEventListener('load', () => {
+//     async function registerSW() {
+//       if ('serviceWorker' in navigator) {
+//         try {
+//           await navigator.serviceWorker.register('./service-worker.js');
+//         } catch (e) {
+//           console.log('ServiceWorker registration failed. Sorry about that.', e);
+//         }
+//       } else {
+//         console.log('Your browser does not support ServiceWorker.');
+//       }
+//     }
+//     registerSW();
+//   });
+// }
 
 
 // >>> Polyfills START
